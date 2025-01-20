5f1be780746bb9478cb903c4f10d243514baad7f
Ivan <ivan.castro.c@gmail.com>
2025-01-20 12:53:43 -0500

    Code cleanup 6

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -3,11 +3,11 @@ from .models import CarMake, CarModel
 
 def initiate():
     car_make_data = [
-        {"name":"NISSAN", "description":"Great cars. Japanese technology"},
-        {"name":"Mercedes", "description":"Great cars. German technology"},
-        {"name":"Audi", "description":"Great cars. German technology"},
-        {"name":"Kia", "description":"Great cars. Korean technology"},
-        {"name":"Toyota", "description":"Great cars. Japanese technology"},
+        {"name": "NISSAN", "description": "Great cars. Japanese technology"},
+        {"name": "Mercedes", "description": "Great cars. German technology"},
+        {"name": "Audi", "description": "Great cars. German technology"},
+        {"name": "Kia", "description": "Great cars. Korean technology"},
+        {"name": "Toyota", "description": "Great cars. Japanese technology"},
     ]
 
     car_make_instances = []
@@ -32,7 +32,7 @@ def initiate():
       {"name": "Corolla", "type": "Sedan", "year": 2023, "car_make": car_make_instances[4]},
       {"name": "Camry", "type": "Sedan", "year": 2023, "car_make": car_make_instances[4]},
       {"name": "Kluger", "type": "SUV", "year": 2023, "car_make": car_make_instances[4]},
-        # Add more CarModel instances as needed
+    # Add more CarModel instances as needed
     ]
 
     for data in car_model_data:
