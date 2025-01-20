caae4403b589379ddaaa3e1f7b8d2cf74fcd3584
Ivan <ivan.castro.c@gmail.com>
2025-01-20 13:51:16 -0500

    Code cleanup 15

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,4 +1,3 @@
-/*jshint esversion: 8 */
 import LoginPanel from "./components/Login/Login"
 import RegisterPanel from "./components/Register/Register"
 import { Routes, Route } from "react-router-dom";
